db1c482e3c55127ad935d30255c5f85e73300766
anosyrev <anosyrev@avinfors.ru>
2018-11-09 12:42:15 +0300

    task #6 Очищаем место под redux

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import BooksTable from './containers/Table';
-import {BasicExample, Home} from './containers/BasicRouterExample';
 
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<Router>
-					<div>
-						<nav>
-							<Link to="/">Home</Link>
-						</nav>
-						<nav>
-							<Link to="/basicExample">BasicExample</Link>
-						</nav>
-						<nav>
-							<Link to="/table">BooksTable</Link>
-						</nav>
-						<hr />
-						<Route exact path="/" component={Home} />
-						<Route path="/basicExample" component={BasicExample} />
-						<Route path="/table" component={BooksTable} />
-					</div>
-				</Router>
-			</div>
+			<h1>HALLO</h1>
 		);
 	}
 }
