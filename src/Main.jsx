9fd601b19925005e808a08e25e6b770cdee2cf23
anosyrev <anosyrev@avinfors.ru>
2019-03-07 10:39:24 +0300

    t-10 Навели чистоту в проекте. Начинаем разрабатывать "коробку".

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import Primary from './components/Primary.jsx';
-import LoginContainer from './containers/LoginContainer.jsx';
-import News from './components/News.jsx';
 // import RedirectToMain from './components/RedirectToMain.jsx';
-import Profile from './components/Profile.jsx';
 
 class Main extends Component { // eslint-disable-line
 	render() {
 		const isAuthorised = this.props.isAuthorised;
 		return (
 			<div id="content">
-				<div/>
+				{/*<div/>
 					<Switch>
-						<Route exact path="/" component={Primary}/>
 						<Route path="/login" component={LoginContainer} />
-						<Route exact path="/news" component={News} />
-						<Route path="/profile" component={ (isAuthorised) ? Profile : LoginContainer} />
-						<Route
-							render={() =>
-								isAuthorised ? (
-									<Redirect to="/profile" />
-								) : (
-									<LoginContainer />
-								)
-							}
-						/>
-					</Switch>
+					</Switch>*/}
 			</div>
 		);
 	}
--- src/datas/NewsData.js
+++
@@ -1,8 +0,0 @@
-export const NewsData =  [
-    {id: "0", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
-    {id: "1", content: "Quisque imperdiet non lectus sit amet finibus. Nulla arcu ipsum, auctor a placerat accumsan, tristique vel dui."},
-    {id: "2", content: "Nullam et interdum orci, sed porttitor enim. Donec diam tellus, pulvinar nec orci eu, scelerisquepharetra tellus. Vivamus varius orci vitae rhoncus tincidunt."},
-    {id: "3", content: "Mauris at ex quis tellus elementum eleifend eget id ex."},
-    {id: "4", content: "Praesent nec hendrerit ex, eget pharetra est."},
-    {id: "5", content: "Vestibulum imperdiet odio vel magna interdum scelerisque."}
-  ];
